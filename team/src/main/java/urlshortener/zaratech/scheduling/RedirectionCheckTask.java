db099518fbdaeb61503a15a359ad99da62dcbcc1
juan-vg <643821@unizar.es>
2017-01-15 18:36:15 +0100

    Add support for async redirection check

--- team/src/main/java/urlshortener/zaratech/scheduling/RedirectionCheckTask.java
+++ team/src/main/java/urlshortener/zaratech/scheduling/RedirectionCheckTask.java
@@ -3,16 +3,25 @@ package urlshortener.zaratech.scheduling;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import urlshortener.common.domain.ShortURL;
+import urlshortener.common.repository.ShortURLRepository;
+import urlshortener.zaratech.core.RedirectionManager;
+
 public class RedirectionCheckTask implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(RedirectionCheckTask.class);
+    
+    private ShortURLRepository shortURLRepository;
+    private ShortURL su;
 
-    public RedirectionCheckTask() {
-
+    public RedirectionCheckTask(ShortURLRepository shortURLRepository, ShortURL su) {
+        this.shortURLRepository = shortURLRepository;
+        this.su = su;
     }
 
     @Override
     public void run() {
-
+        logger.info("--- STARTING REDIRECTION CHECK FOR URI " + su.getTarget() + " ---");
+        RedirectionManager.checkNorFromUri(shortURLRepository, su);
     }
 }
