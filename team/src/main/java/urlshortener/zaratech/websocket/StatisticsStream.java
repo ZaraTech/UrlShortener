63ad48f9df1ae2d33e266b677c92fc40acb7fc45
aitorsanz <aitor_sanz@hotmail.com>
2017-01-16 01:46:29 +0100

    Fix date problems

--- team/src/main/java/urlshortener/zaratech/websocket/StatisticsStream.java
+++ team/src/main/java/urlshortener/zaratech/websocket/StatisticsStream.java
@@ -1,5 +1,7 @@
 package urlshortener.zaratech.websocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -13,12 +15,15 @@ import urlshortener.zaratech.websocket.StatisticHandler;
 @EnableWebSocket
 public class StatisticsStream implements WebSocketConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(StatisticsStream.class);
+
     @Autowired
     private StatisticHandler statisticHandler;  // The WebSocketHandler that manages the events related to the socket
 
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        logger.info("Registrando Websocket StatisticStream");
         registry.addHandler(statisticHandler, "/stats-stream").setAllowedOrigins("*");
     }
 }
--- team/src/main/resources/static/js/app-statistic-stream.js
+++ team/src/main/resources/static/js/app-statistic-stream.js
@@ -1,11 +1,7 @@
 $(document).ready(
-
     function() {
-        var ws = new WebSocket("ws://" + window.location.host + "/stats-stream");
-        ws.onmessage = function(res) {
-            getStatistics();
-        };
 
+        getStatistics();
         $('#form').submit(
             function(event){
                 event.preventDefault();
@@ -26,6 +22,28 @@ function getStatistics() {
             loadData(msg);
             loadCalendar();
             loadChart(msg);
+        },
+        error : function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Can not connect to the server. Verify Network.';
+            } else if (jqXHR.status == 400) {
+                msg = 'Bad resquest. [HTTP Code 400]';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [HTTP Code 404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error. [HTTP Code 500]';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            $("#result").html(
+                "<div class='alert alert-danger lead'>ERROR: "+ msg +"</div>");
         }
     } );
 }
@@ -39,12 +57,34 @@ function getStatisticsFiltered(data) {
             loadData(msg);
             loadCalendar();
             loadChart(msg);
+        },
+        error : function (jqXHR, exception) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Can not connect to the server. Verify Network.';
+            } else if (jqXHR.status == 400) {
+                msg = 'Bad resquest. [400]';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error. [500]';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            $("#result").html(
+                "<div class='alert alert-danger lead'>ERROR: "+ msg +"</div>");
         }
     } );
 }
 
 function loadData(msg) {
-
+    console.log('Mostrando estadisticas ');
     var data="";
     data+=
         "<div class='text-left'><table class='table table-hover table-bordered' id='data'>"
@@ -62,7 +102,8 @@ function loadData(msg) {
         var versionLen=msg.versionList[i].length;
         for(var j=0;j<versionLen;j++){
             // save browsers data
-            data += "<tr><td>" +msg.browserList[i]+"</td>"+"<td>" +msg.versionList[i][j]+"</td>"+"<td>" +((msg.clicksforversion[i][j]/msg.clicks)*100).toFixed(2)+" %</td></tr>";
+            data += "<tr><td>" +msg.browserList[i]+"</td>"+"<td>" +msg.versionList[i][j]+"</td>"+"<td>"
+                +((msg.clicksforversion[i][j]/msg.clicks)*100).toFixed(2)+" %</td></tr>";
         }
 
     }data+="</tbody></table></div>";
@@ -90,59 +131,110 @@ function loadCalendar(){
     $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
     $( "#datepicker2" ).datepicker({ dateFormat: 'yy-mm-dd' });
 }
+
 function loadChart(msg){
 
-    var info = msg.jsonOs;
-    var info2 = msg.jsonVersion;
+    var infoOs = msg.jsonOs;
+    var infoBrowser = msg.jsonVersion;
 
-    var chart2 = new Highcharts.Chart({
+    (function (Highcharts) {
+        Highcharts.wrap(Highcharts.seriesTypes.pie.prototype, 'render', function (proceed) {
+
+            var chart = this.chart,
+                center = this.center || (this.yAxis && this.yAxis.center),
+                titleOption = this.options.title,
+                box;
+
+            proceed.call(this);
+
+            if (center && titleOption) {
+                box = {
+                    x: chart.plotLeft + center[0] - 0.5 * center[2],
+                    y: chart.plotTop + center[1] - 0.5 * center[2],
+                    width: center[2],
+                    height: center[2]
+                };
+                if (!this.title) {
+                    this.title = this.chart.renderer.label(titleOption.text)
+                        .css(titleOption.style)
+                        .add()
+                }
+                var labelBBox = this.title.getBBox();
+                if (titleOption.align == "center")
+                    box.x -= labelBBox.width/2;
+                else if (titleOption.align == "right")
+                    box.x -= labelBBox.width;
+                this.title.align(titleOption, null, box);
+            }
+        });
+
+    } (Highcharts));
+
+    // Clicks chart
+    var chart = new Highcharts.Chart({
         chart: {
-            renderTo: 'chart2',
+            renderTo: 'chart',
             type: 'pie'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
+        },
+        legend: {
+            enabled: true,
+            borderWidth: 1,
+            borderColor: 'gray',
+            align: 'center',
+            verticalAlign: 'top',
+            layout: 'horizontal',
+            x: 0, y: 50
         },
         plotOptions: {
             pie: {
                 allowPointSelect: true,
+                showInLegend: true,
                 cursor: 'pointer',
                 dataLabels: {
                     enabled: true,
+                    distance: -20,
+                    format: '<b>{point.name}</b>: {point.percentage:.2f} %',
                     style: {
                         color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                     }
-                }
+                },
+                size: '80%'
             }
         },
         series: [{
-            data: info2,
+            type: 'pie',
+            data: infoOs,
             sliced: true,
             selected: true,
-            name:"Versions"
-        }]
-    });
-    chart2.setTitle({text: "Browsers"});
-    var chart1 = new Highcharts.Chart({
-        chart: {
-            renderTo: 'chart1',
-            type: 'pie'
+            startAngle: 45,
+            name:"Clicks",
+            colors: ['#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+            title: {
+                align: 'left',
+                text: '<b>Operative System</b><br>Clicks stats',
+                verticalAlign: 'top',
+                y: -40
+            },
+            center: ['30%', '50%']
         },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            data: info,
-            sliced: true,
-            selected: true,
-            name:"Os"
-        }]
+            {
+                type: 'pie',
+                data: infoBrowser,
+                sliced: true,
+                selected: true,
+                startAngle: 45,
+                name:"Clicks",
+                title: {
+                    align: 'right',
+                    text: '<b>Browser Version</b><br>Clicks stats',
+                    verticalAlign: 'top',
+                    y: -40
+                },
+                center: ['70%', '50%']
+            }]
     });
-    chart1.setTitle({text: "Operative System"});
+    chart.setTitle({text: ""});
 }
